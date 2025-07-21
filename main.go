2fc85df0e2d56f303aa0c960ca1de9d0fd1350d7
muhamadandre10 <andrepriyanto95@gmail.com>
2025-07-21 20:50:04 +0700

    create new server

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Server running on port 8080")
+
+	srv.ListenAndServe()
+
+}
